5a7451105d268961e0ee6f96aff6365f28d4f1cb
Ravi Raghavan <ravi0308raghavan@gmail.com>
2023-12-15 13:29:52 -0500

    Matched Paper Results

--- Reinforcement_Learning_LQ_Nash_Strategies.m
+++ Reinforcement_Learning_LQ_Nash_Strategies.m
@@ -4,34 +4,64 @@
 %Clear All Variables in our Workspace
 clear all
 
+%Inertia Matrix
+J_ast = [90 10 5; 
+         10 50 7;
+         5 7 50];
+
+%Eigendecomposition of Inertia Matrix
+[V, D] = eig(J_ast);
+eigenvalues = diag(D);
+
+%Get the orthogonal rotation matrix
+rotation_matrix = V;
+
+%We are given C1, C2, and C3
+C1 = eye(3);
+C2 = [0.8829 0 0.4695;
+      0.4695 0 -0.8829;
+      0 1 0];
+
+C3 = [0.7986 -0.6018 0;
+      -0.6018 -0.7986 0;
+      0 0 -1];
+
+%Solve for G1, G2, and G3
+G1 = rotation_matrix' * C1;
+G2 = rotation_matrix' * C2;
+G3 = rotation_matrix' * C3;
+
+%Solve for the inertia matrix of the combined spacecraft
+J = rotation_matrix' * J_ast * rotation_matrix;
+
+%Get Jx, Jy, and Jz
+Jx = J(1, 1);
+Jy = J(2, 2);
+Jz = J(3, 3);
+
+%w0 Value as given to us
+w0 = 7.292 * 10^(-5);
+
+%Storage values
+v1 = (-1 * ((Jy - Jz) / Jx) * w0^2);
+v2 = (-1 * ((Jy - Jz - Jx) / Jx) * w0);
+v3 = (-1 * ((Jy - Jx) / Jz) * w0^2);
+v4 = (1 * ((Jy - Jz - Jx) / Jz) * w0);
+
+%Set up A and B matrices
+A = [0 0 0 1 0 0;
+    0 0 0 0 1 0;
+    0 0 0 0 0 1;
+    v1 0 0 0 0 v2;
+    0 0 0 0 0 0;
+    0 0 v3 v4 0 0];
+
+B1 = vertcat(zeros(3,3), inv(J) * G1);
+B2 = vertcat(zeros(3,3), inv(J) * G2);
+B3 = vertcat(zeros(3,3), inv(J) * G3);
+
 %Initialize the state value
-x = [0.0873, 0.0524, 0.0698, 0,0,0]';
-
-%Note: Currently, values of A, B1, B2, B3, Q Matrices, R matrices were set
-%to bullshit values. This needs to be verified b4 submission!
-
-%Set value of A
-A = [-0.4125 -0.0248 0.0741 0.0089 0 0;
-     101.5873 -7.2651 2.7608 2.8068 0 0;
-     0.0704 0.0085 -0.0741 -0.0089 0 0.02;
-     0.0878 0.2672 0 -0.3674 0.0044 0.3962;
-     -1.8414 0.099 0 0 -0.0343 -0.033;
-     0 0 0 -359 187.5364 -87.0316];
-
-%Set value of B1
-B1 = [-0.0042 -1.036 0.0042 0.1261 0 0;
-      0.0064 1.5849 0 0 -0.0168 0;
-      0.0064 1.5849 0 0 -0.0168 0]';
-
-%Set value of B2
-B2 = [-0.0153 -1.526 0.0032 0.3261 0 0;
-      0.0364 1.7 0 0 -0.2 0;
-      0.0064 1.5849 0 0 -0.0168 0]';
-
-%Set value of B3
-B3 = [-0.0153 -1.526 0.0032 0.3261 0 0;
-      0.0364 1.7 0 0 -0.2 0;
-      0.0064 1.5849 0 0 -0.0168 0]';
+x_initial = [0.0873, 0.0524, 0.0698, 0,0,0]';
 
 %Get values of Q matrices
 Q1=0.005*eye(6);
@@ -78,15 +108,15 @@ P3_values{1} = P3;
 
 %Store Performance Criterion Values for each Player
 J1_values = cell(1, iterations + 1);
-J1_values{1} = x' * P1 * x;
+J1_values{1} = x_initial' * P1 * x_initial;
 
 J2_values = cell(1, iterations + 1);
-J2_values{1} = x' * P2 * x;
+J2_values{1} = x_initial' * P2 * x_initial;
 
 J3_values = cell(1, iterations + 1);
-J3_values{1} = x' * P3 * x;
+J3_values{1} = x_initial' * P3 * x_initial;
 
-
+%Conduct Lyapunov Iterations
 for i = 1:iterations
     P1_updated = lyap2((A-S1*P1-S2*P2-S3*P3)',Q1+P1*S1*P1+P2*S21*P2+P3*S31*P3);
     P2_updated = lyap2((A-S1*P1-S2*P2-S3*P3)',Q2+P1*S12*P1+P2*S2*P2+P3*S32*P3);
@@ -100,7 +130,56 @@ for i = 1:iterations
     P2_values{i + 1} = P2_updated;
     P3_values{i + 1} = P3_updated;
 
-    J1_values{i + 1} = x' * P1 * x;
-    J2_values{i + 1} = x' * P2 * x;
-    J3_values{i + 1} = x' * P3 * x;
+    J1_values{i + 1} = x_initial' * P1 * x_initial;
+    J2_values{i + 1} = x_initial' * P2 * x_initial;
+    J3_values{i + 1} = x_initial' * P3 * x_initial;
 end
+
+%Define each policy accordingly
+F1 = inv(R11) * B1' * P1;
+F2 = inv(R22) * B2' * P2;
+F3 = inv(R33) * B3' * P3;
+
+u1 = @(x) -F1 * x;
+u2 = @(x) -F2 * x;
+u3 = @(x) -F3 * x;
+
+%Define time span
+time = [0, 200];
+
+% Define the system dynamics function
+sys_dynamics = @(t, x) A * x + B1 * u1(x) + B2 * u2(x) + B3 * u3(x);
+[t, x] = ode45(sys_dynamics, time, x_initial);
+
+
+figure;  % Create a new figure
+hold on;  % Enable hold to overlay plots on the same figure
+
+plot(t, x(:, 1), 'r-', 'LineWidth', 2);  % Plot the first line in red
+plot(t, x(:, 2), 'b-', 'LineWidth', 2);  % Plot the second line in green with dashed line
+plot(t, x(:, 3), 'g-', 'LineWidth', 2);  % Plot the third line in blue with dash-dot line
+
+hold off;  % Disable hold to stop overlaying plots
+
+% Add labels and legend
+xlabel('time/s');
+ylabel('attitude/rad');
+legend('γ', 'θ', 'ψ');
+
+
+figure;  % Create a new figure
+hold on;  % Enable hold to overlay plots on the same figure
+
+plot(t, x(:, 4), 'r-', 'LineWidth', 2);  % Plot the first line in red
+plot(t, x(:, 5), 'b-', 'LineWidth', 2);  % Plot the second line in green with dashed line
+plot(t, x(:, 6), 'g-', 'LineWidth', 2);  % Plot the third line in blue with dash-dot line
+
+hold off;  % Disable hold to stop overlaying plots
+
+% Add labels and legend
+xlabel('time/s');
+ylabel('w/rad/s');
+legend({'$w_x$', '$w_y$', '$w_z$'}, 'Interpreter', 'latex', 'FontSize', 12);
+
+
+
